ca4cbad700e72ccfea1899497f08ae265d1ce4cc
SanjithSibi <sanjithmani20@gmail.com>
2023-04-11 17:44:10 +0530

    - Security Updated

--- free_gift/App/controller/Base.php
+++ free_gift/App/controller/Base.php
@@ -2,18 +2,18 @@
 
 namespace Fg\App\controller;
 defined('ABSPATH') or exit();
-
-
 class Base
 {
-    public function addFreeGiftTab($tabs){
+    public function
+    addFreeGiftTab($tabs){
+
         if(! is_array($tabs)) {
             return $tabs;
         }
-        $tabs['free-gift'] = array(
-            'label' => __('Free Gifts', 'free-gift'),
+        $tabs['fg-free-gift'] = array(
+            'label' => __('Free Gifts', 'fg-free-gift'),
             'priority' => 50,
-            'target' => 'Free_gift',
+            'target' => 'fg_free_gift',
         );
         return $tabs;
     }
@@ -22,79 +22,93 @@ class Base
         global  $post;
         if(! isset($post->ID) || (! is_numeric($post->ID))){ return ; }
         $product_id = $post->ID;
-        $checkbox_value = get_post_meta( $product_id, '_checkbox_meta', true );
-        $value = (isset($checkbox_value) && $checkbox_value == 'on') ? 'checked="checked"' : '';
-        ?>
-        <div id="Free_gift" class="panel woocommerce_options_panel hidden">
-            <input type="checkbox" id="my_checkbox" name="my_checkbox" value="on" <?php echo $value; ?>>
-        </div>
-
-        <?php
-
+        $checkbox_value = get_post_meta( $product_id, '_fg_checkbox_meta', true );
+        $value = (!empty($checkbox_value) && $checkbox_value == 'on') ? 'checked="checked"' : '';
+        wc_get_template( 'admin_product.php', array('Value' => $value), '',WP_PLUGIN_DIR . '/free_gift/App/view/' );
     }
     function saveCheckboxValueToDatabase( $post_id ) {
-        $checkbox_value = isset($_POST['my_checkbox']) ? sanitize_text_field($_POST['my_checkbox']):'';
-        if(!empty($checkbox_value) && $checkbox_value=='on') {
-            update_post_meta($post_id, '_checkbox_meta', $checkbox_value);
+        $checkbox_value = isset($_POST['my_checkbox']) ?  sanitize_text_field($_POST['my_checkbox']):'off';
+        if(!empty($checkbox_value) ) {
+            update_post_meta($post_id, '_fg_checkbox_meta', $checkbox_value);
         }
     }
     function checkCartItems()
     {
-        if (!function_exists('WC')) return;
-        $cart = WC()->cart;
+
+        if(!function_exists('WC') || ! is_object(WC()->cart) || ! method_exists(WC()->cart,'get_cart')){return;}
+            $cart = WC()->cart->get_cart();
+
+        if(! isset($cart) || ! is_array($cart)){return;}
         $parent_key = array();
         $gift_id = array();
         $gift_key=array();
-        foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+        foreach ($cart as $cart_item_key => $cart_item) {
+            if(!is_array($cart_item)){continue;}
+            if(!isset($cart_item['product_id'])){continue;}
             $product_id = $cart_item['product_id'];
-            $checkbox_value = get_post_meta($product_id, '_checkbox_meta', true);
-            if (!isset($cart_item['variation']['bogo']) && $checkbox_value == 'on') {
-                $parent_key[] = $cart_item_key;
+            $checkbox_value = get_post_meta($product_id, '_fg_checkbox_meta', true);
+            if ( $checkbox_value === false || $checkbox_value === '' ) {continue;}
+            if ((!isset($cart_item['variation'])|| !isset($cart_item['variation']['bogo'])) && $checkbox_value == 'on'
+            ) {
+                if(isset($parent_key) && is_array($parent_key)) {
+                    $parent_key[] = $cart_item_key;
+                }
             }
-            if (isset($cart_item['variation']['bogo'])) {
-                 $gift_key[]=$cart_item_key;
-                $gift_id[] = $cart_item['variation']['bogo'];
+            if (isset($cart_item['variation']) && isset($cart_item['variation']['bogo'])) {
+                if(isset($gift_key) && is_array($gift_key)){
+                 $gift_key[]=$cart_item_key;}
+                if(isset($gift_id) && is_array($gift_id)){
+                $gift_id[] = $cart_item['variation']['bogo'];}
             }
         }
-        if (!empty($parent_key)) {
+        if (!empty($parent_key) && is_array($parent_key)) {
             foreach ($parent_key as $key) {
-                $cart_item = $cart->get_cart_item($key);
+                if (! method_exists( WC()->cart, 'get_cart_item' ) ) {return;}
+                $cart_item = WC()->cart->get_cart_item($key);
+                if(!isset($cart_item['product_id']) || !isset($cart_item['quantity'])){continue;}
                 $product_id = $cart_item['product_id'];
-                $quant = $cart_item['quantity'];
+                $parent_quantity = $cart_item['quantity'];
                 if (empty($gift_id) || !in_array($key, $gift_id)) {
-                    $quantity = $quant;
+                    $quantity = $parent_quantity;
+                    if (! method_exists( WC()->cart, 'add_to_cart' ) ) {return;}
                     WC()->cart->add_to_cart($product_id, $quantity, array(), array('bogo' => $key));
                 }
-                                }
             }
-        foreach ($gift_key as $gift_product_key){
-            $cart_item = $cart->get_cart_item($gift_product_key);
-            $parent_key=$cart_item['variation']['bogo'];
-            if (!WC()->cart->find_product_in_cart($parent_key)) {
-                WC()->cart->remove_cart_item($gift_product_key);
+            }
+        if(!empty($gift_key) && is_array($gift_key)) {
+            foreach ($gift_key as $gift_product_key) {
+                if (! method_exists( WC()->cart, 'get_cart_item' ) ) {return;}
+                $cart_item =  WC()->cart->get_cart_item($gift_product_key);
+                if(!isset($cart_item['variation']) || !isset($cart_item['variation']['bogo'])){continue;}
+                $parent_key = $cart_item['variation']['bogo'];
+                if (!WC()->cart->find_product_in_cart($parent_key)) {
+                    WC()->cart->remove_cart_item($gift_product_key);
+                }
             }
         }
-
         }
         function customPrice($cart_object)
         {
             if (!isset($cart_object) || !is_object($cart_object)) {
                 return;
             }
+            if(! is_object($cart_object) || ! method_exists(WC()->cart,'get_cart')){return;}
             foreach ($cart_object->get_cart() as $key => $value) {
-                if (isset($value['variation']['bogo'])) {
+                if(!is_array($value)){continue;}
+                if ( !isset($value['variation']) ||! isset($value['variation']['bogo'])) {continue;}
                     $price = 0;
+                if (!isset($value['data'])|| !is_object($value['data']) || ! method_exists( $value['data'], 'set_price' ) ) {continue;}
                     $value['data']->set_price($price);
                     $parent_key = $value['variation']['bogo'];
-                    $cart = WC()->cart;
-                    $cart_item = $cart->get_cart_item($key);
+                if (!function_exists('WC') || !is_object(WC()->cart) || ! method_exists( WC()->cart, 'get_cart_item' )) return;
+                $cart_item = WC()->cart->get_cart_item($key);;
+                if(! isset($cart_item['quantity'])){continue;}
                     $gift_quantity = $cart_item['quantity'];
-                    $cart_item = $cart->get_cart_item($parent_key);
+                    $cart_item = WC()->cart->get_cart_item($parent_key);
                     $parent_quantity = $cart_item['quantity'];
                     $quantity = $parent_quantity;
-                    if ($gift_quantity != $parent_quantity) {
+                    if ($gift_quantity != $parent_quantity && method_exists( WC()->cart, 'set_quantity' )) {
                         WC()->cart->set_quantity($key, $quantity);
-                    }
                 }
             }
         }
--- free_gift/free_gift.php
+++ free_gift/free_gift.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Plugin name:Buy X Get X
  * Plugin Description:Adding gifts to the products
  * version:1.0
@@ -7,12 +7,12 @@
  * Text Domain: free-gift
  */
 
-use Fg\App\Router;
 
 defined('ABSPATH') or exit();
 if(!file_exists(WP_PLUGIN_DIR.'/free_gift/vendor/autoload.php')) return;
 require_once WP_PLUGIN_DIR.'/free_gift/vendor/autoload.php';
+defined("BOGO_PATH") or define("BOGO_PATH",plugin_dir_url(__FILE__));
 
 if((!class_exists('Fg\App\Router'))  || (!method_exists(\Fg\App\Router::class,'hooks'))) return;
-$router=new Router();
+$router=new Fg\App\Router();
 $router->hooks();
