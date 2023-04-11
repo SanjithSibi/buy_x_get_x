356d26beeb0ebb4f37b6c73d611cb7f118bb2ae9
SanjithSibi <sanjithmani20@gmail.com>
2023-04-11 17:48:47 +0530

    - Security Updated

---
+++ free_gift/App/view/admin_product.php
@@ -0,0 +1,6 @@
+<div id="fg_free_gift" class="panel woocommerce_options_panel hidden">
+
+    <input type="checkbox" id="my_checkbox" name="my_checkbox" value="on" <?php echo isset($Value) && !empty($Value) ? $Value:'';?> />
+    <?php _e('Click here to add gift', 'fg-free-gift'); ?>
+
+</div>
